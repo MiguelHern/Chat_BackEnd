3b47d9d9b4eb59dbf67f0360a07584fc2215a360
MiguelHern <al066382@uacam.mx>
2024-02-19 10:35:45 -0600

    first commit

---
+++ Core/Features/Usuarios/Command/Login.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Domain.Exceptions;
+using Core.Domain.Services;
+using Core.Infraestructure.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Features.Usuarios.Command;
+
+public record Login : IRequest<LoginResponse>
+{
+    /// <example>
+    /// samuelopez
+    /// </example>
+    [Required]
+    public string UserName { get; set; }
+    
+    [Required]
+    public string Contrasena { get; set; }
+}
+
+public class LoginCommandHandler : IRequestHandler<Login, LoginResponse>
+{
+    private readonly IAuthService _authService;
+    
+    public LoginCommandHandler(IAuthService authService)
+    {
+        _authService = authService;
+    }
+    
+    public async Task<LoginResponse> Handle(Login request, CancellationToken cancellationToken)
+    {
+        //Valida que no esten vacias
+        if(string.IsNullOrEmpty(request.UserName) || string.IsNullOrEmpty(request.Contrasena))
+            throw new BadRequestException("Usuario y contraseña son obligatorios");
+        
+        //Si cumple con las validaciones se procede a autenticar
+        var token = await _authService.AuthenticateAsync(request.UserName, request.Contrasena);
+        
+        if(token == null)
+            throw new NotFoundException("Usuario no encontrado");
+        
+        //Respuesta
+        return new LoginResponse()
+        {
+            Token = token,
+        };
+    }
+}
+
+public record LoginResponse
+{
+    public string Token { get; set; }
+}
